278cdcbf3258d26f0e730cee40c6c7afbee741d6
BoneNon <non355@gmail.com>
2024-08-30 00:03:03 +0700

    add dotnet

---
+++ dotnet-clean-architecture/Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,35 @@
+﻿using Application.Profiles;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application {
+    public static class ApplicationServiceRegistration {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services) {
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddTransient<IdentityUser>();
+
+            services.AddAutoMapper(cfg => cfg.AddProfileRegistration());
+
+            return services;
+        }
+
+        public static IMapperConfigurationExpression AddProfileRegistration(this IMapperConfigurationExpression mapper) {
+            mapper.AddProfile<CategoryProfile>();
+            mapper.AddProfile<BlogPostProfile>();
+            mapper.AddProfile<BlogImageProfile>();
+            return mapper;
+        }
+    }
+}
